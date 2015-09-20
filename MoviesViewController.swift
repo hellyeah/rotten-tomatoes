1328e586b0fcf8a6e975fc3566f6bb47af2b8a02
Dave Fontenot <dave@hackmatch.com>
2015-09-20 03:14:58 -0400

    added tableView and linked up data

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //prints json data from any url
         fetchData("https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")
 
         // Do any additional setup after loading the view.
     }
-
+    
     func fetchData(urlString: String) -> Void {
         let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)
         let task = session.dataTaskWithURL(url!) {
             (data, response, error) -> Void in
-
+            
             if error != nil {
                 print(error!.localizedDescription)
             }
@@ -32,21 +35,48 @@ class MoviesViewController: UIViewController {
                 //let error: NSError?
                 do {
                     let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! NSDictionary
-                    print(jsonData)
+                    //print(jsonData)
+                    self.movies = jsonData["movies"] as! [NSDictionary]
+                    
+                    print(self.movies)
+                    self.tableView.reloadData()
                     // Do Stuff
-
+                    
                 } catch {
                     // handle error
                 }
             }
+            self.tableView.dataSource = self
+            self.tableView.delegate = self
         }
-
+        
         task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (movies != nil) {
+            return movies!.count
+        } else {
+            return 0
+        }
+
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let movie = movies![indexPath.row]
+        
+        cell.textLabel?.text = movie["title"] as? String
+        
+        return cell
     }
     
 
@@ -59,5 +89,7 @@ class MoviesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
 }
